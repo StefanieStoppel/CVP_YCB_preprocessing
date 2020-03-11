6ae8d0c0c50209a308e526281734753b0c3e76d0
Stefanie Stoppel <s.stoppel@gmx.de>
2020-03-11 14:26:35 +0100

    Display nontextured.ply

--- display_point_cloud.py
+++ display_point_cloud.py
@@ -1,4 +1,22 @@
 import open3d as o3d
+import h5py
+import numpy as np
 
-pcd = o3d.io.read_point_cloud("./data/006_mustard_bottle/clouds/pc_NP2_NP5_15.ply")
+# Open file containing calibration parameters
+f = h5py.File('./data/006_mustard_bottle/calibration.h5', 'r')
+print(list(f.keys()))
+
+# Get homogeneous matrix from camera NP2 to NP5
+H_NP2_from_NP5 = np.array(f['H_N2_from_NP5'])
+#print(H_NP2_from_NP5)
+#transform = np.linalg.inv(H_NP2_from_NP5)
+
+# Read point cloud file
+pcd = o3d.io.read_point_cloud("./data/006_mustard_bottle/clouds/nontextured.ply")
+print(np.asarray(pcd.points).shape)
+
+# Apply transform to PC
+#pcd.transform(H_NP2_from_NP5)
+
+# Visualize original point cloud and transformed point cloud
 o3d.visualization.draw_geometries([pcd])
